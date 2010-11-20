f9f2c81a43490ac48aba46578567aa42e43a9f0f
John Dowd <john.dowd@PrecisionAds.com>
2010-11-19 20:44:32 -0500

    Can now destroy users (as admins)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20101110210015
+# Schema version: 20101119213448
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
